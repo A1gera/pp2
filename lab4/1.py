ad60352a3a60ef0fc5d28db2cc6a05f75b41ba8f
A1gera <a_baibakty@kbtu.kz>
2023-02-27 21:17:11 +0600

    lab4

--- lab4/1.py
+++ lab4/1.py
@@ -10,15 +10,15 @@ DN                                                 Description           Speed  
     """)
 imdata = json_data["imdata"]
 for item in imdata:
-        nest_item = item["l1PhysIf"]
-        attribs = nest_item["attributes"]
-        dn = attribs["dn"]
-        speed = attribs["speed"]
-        mtu = attribs["mtu"]
-        output = ""
-        if(len(dn) == 42):
-            output += dn + "                              " + speed + "   " + mtu
-        else:
-            output += dn + "                               " + speed + "   " + mtu
+    nest_item = item["l1PhysIf"]
+    attribs = nest_item["attributes"]
+    dn = attribs["dn"]
+    speed = attribs["speed"]
+    mtu = attribs["mtu"]
+    output = ""
+    if(len(dn) == 42):
+        output += dn + "                              " + speed + "   " + mtu
+    else:
+        output += dn + "                               " + speed + "   " + mtu
        
-        print(output)
+    print(output)
